3c50ae79cfaec87c8b946f80212c4f2c6d56844d
shabil15 <mohammedshabil15@gmail.com>
2024-03-20 10:54:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+//Infer Types ()
+// let userName = 'Shabil';
+// let users = 10000;
+//Defining Types(explicit Types )
+var userName = 'Shabil';
+var users = 24534;
+var skills = ['js', 'html', 'css'];
+var prices = [123, 34343, 234, 34];
---
+++ index.ts
@@ -0,0 +1,9 @@
+//Infer Types ()
+// let userName = 'Shabil';
+// let users = 10000;
+
+//Defining Types(explicit Types )
+let userName:string = 'Shabil';
+let users: number = 24534;
+let skills:string[] = ['js','html','css']
+let prices:number[]= [123,34343,234,34];
